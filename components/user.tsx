bca37590bd848abf580b3e74e27f3723819849ed
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-02-27 22:05:58 +0900

    shouldComponentUpdateによるパフォーマンスアップ

--- components/user.tsx
+++ components/user.tsx
@@ -7,18 +7,26 @@ interface UserProps {
   userSelected: () => void;
 }
 
-const User: SFC<UserProps> = (props) => {
-  console.log("ユーザーをレンダリング中:", [props.name, props.highlighted]);
-  return <div>
-    <h3
-      style={{fontStyle: props.highlighted ? 'italic' : 'normal'}}
-      onClick={event => {
-        props.userSelected();
-      }}
-    >
-      {props.name}
-    </h3>
-  </div>;
-};
+class User extends React.Component<UserProps, {}> {
+
+  shouldComponentUpdate(nextProps: UserProps) {
+    return nextProps.name !== this.props.name || nextProps.highlighted !== this.props.highlighted;
+  }
+
+  render() {
+    const { props } = this;
+    console.log("ユーザーをレンダリング中:", [props.name, props.highlighted]);
+    return <div>
+      <h3
+        style={{fontStyle: props.highlighted ? 'italic' : 'normal'}}
+        onClick={event => {
+          props.userSelected();
+        }}
+      >
+        {props.name}
+      </h3>
+    </div>;
+  };
+}
 
 export default User;
